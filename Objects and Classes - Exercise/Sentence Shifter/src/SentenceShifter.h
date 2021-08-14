25cca566adc51778a126c229efa3e42c3e3557b7
Viktor <vidolarez@gmail.com>
2021-08-14 14:56:04 +0300

    Organisational changes

--- Objects and Classes - Exercise/Sentence Shifter/src/SentenceShifter.h
+++ Objects and Classes - Exercise/Sentence Shifter/src/SentenceShifter.h
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <vector>
+#include <iostream>
+#include <deque>
 #include <string>
 #include <sstream>
 
+typedef std::deque <std::string> Words;
 
-class SentenceShifter {
-    public:
-    SentenceShifter(){
-        getline(std::cin, sentences);
+class SentenceShifter
+{
+private:
+    Words _words;
+    size_t _shiftsCount = 0;
+public:
+    void readInput(){
+        std::string line;
+        std::getline(std::cin, line);
+        std::stringstream str(line);
+
+        std::string word;
+        while(str>>word){
+            _words.push_back(word);
+        }
+
+        std::cin >> _shiftsCount;
+        std::cin.ignore();
+    }
+    void shiftsentence(){
+        if(_words.empty()) return;
+        
+        for (size_t i = 0; i < _shiftsCount; i++)
+        {
+            std::string word = _words.back();
+            _words.pop_back();
+            _words.push_front(word);
+        }
+        
     }
 
-    std::string getShiftedSentence() {
-        std::string shifted;
-        std::stringstream str (sentences);
-
+    Words getShiftedSentence() const{
+        return _words;
     }
-    private:
-    std::string sentences;
 };
+
+

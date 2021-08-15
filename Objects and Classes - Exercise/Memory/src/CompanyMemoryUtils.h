d8ed86c88079e22ec043a1ca34fd9c98e8d7c42c
Viktor <vidolarez@gmail.com>
2021-08-15 14:45:52 +0300

    Another set of solutions

---
+++ Objects and Classes - Exercise/Memory/src/CompanyMemoryUtils.h
@@ -0,0 +1,12 @@
+#ifndef COMPANYMEMORYUTILS_H
+#define COMPANYMEMORYUTILS_H
+
+#include "Company.h"
+
+typedef unsigned char byte;
+
+std::vector<Company> readCompaniesFromMemory(byte *mem, int num){
+    std::vector<Company> companies;
+    return companies;
+}
+#endif //COMPANY_MEMORY_UTILS_H
